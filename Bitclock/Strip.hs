7d3efc3ad8ede1e019b9b574a1d2c33cacdcd7e0
Blake Smith <blakesmith0@gmail.com>
2013-10-29 23:20:05 -0500

    runStrip takes mkStrip parameters

--- Bitclock/Strip.hs
+++ Bitclock/Strip.hs
@@ -8,9 +8,9 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Data.LDP8806
 
-runStrip :: Int -> ClockState -> IO ClockState
-runStrip sampleMs cs = do
-         _ <- forkIO $ forever $ mkStrip 64 "/tmp/clock_test" >>= doRead >> threadDelay (sampleMs * 1000)
+runStrip :: Int -> Integer -> String -> ClockState -> IO ClockState
+runStrip sampleMs ledCount fh cs = do
+         _ <- forkIO $ forever $ mkStrip ledCount fh >>= doRead >> threadDelay (sampleMs * 1000)
          return cs
 
     where doRead = runSPI $ liftIO (readClock cs) >>= setLeds
--- Main.hs
+++ Main.hs
@@ -3,4 +3,4 @@ import Bitclock.Strip
 import Bitclock.Web
 
 main :: IO ()
-main = putStrLn "Starting Clock" >> newClock 250 >>= runStrip 500 >>= serveWeb
+main = putStrLn "Starting Clock" >> newClock 250 >>= runStrip 500 64 "/tmp/clock_test" >>= serveWeb
