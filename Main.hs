990eeb6fb418d1897c4b62963ca18dfa9a6dc3ca
Blake Smith <blakesmith0@gmail.com>
2013-10-29 23:11:58 -0500

    Read the clock state and write it to the strip

--- Main.hs
+++ Main.hs
@@ -1,5 +1,16 @@
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.LDP8806
 import Bitclock.Clock
 import Bitclock.Web
 
+runStrip :: Int -> ClockState -> IO ClockState
+runStrip sampleMs cs = do
+         _ <- forkIO $ forever $ mkStrip 64 "/tmp/clock_test" >>= doRead >> threadDelay (sampleMs * 1000)
+         return cs
+
+    where doRead = runSPI $ liftIO (readClock cs) >>= setLeds
+
 main :: IO ()
-main = putStrLn "Starting Clock" >> newClock 250 >>= serveWeb
+main = putStrLn "Starting Clock" >> newClock 250 >>= runStrip 500 >>= serveWeb
