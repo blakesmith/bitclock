9943a804a112bf3f092761e28ef627a901c0c4d5
Blake Smith <blakesmith0@gmail.com>
2013-11-14 17:17:16 -0600

    Determine the color by the time of day

--- Bitclock/Clock.hs
+++ Bitclock/Clock.hs
@@ -27,15 +27,22 @@ readClock = readMVar
 bitValue :: Integer -> Int -> Bool
 bitValue v n = (==) 1 $ (.&.) 1 $ shiftR v n
 
-colorByTimeOfDay :: POSIXTime -> Color
-colorByTimeOfDay t = Color 255 127 0
+colorByTimeOfDay :: Color -> Color -> POSIXTime -> Color
+colorByTimeOfDay c1 c2 t = Color r g b
+                 where r = round ((fromIntegral $ redValue c1) * p + (fromIntegral $ redValue c2) * (1 - p))
+                       g = round ((fromIntegral $ greenValue c1) * p + (fromIntegral $ greenValue c2) * (1 - p))
+                       b = round ((fromIntegral $ blueValue c1) * p + (fromIntegral $ blueValue c2) * (1 - p))
+                       p = fromIntegral curSec / (fromIntegral $ round posixDayLength)
+                       curSec = (flip mod (round 20) . fromIntegral . round) t
 
 getTimestampColors :: Int -> POSIXTime -> [Color]
 getTimestampColors ledCount pt = map color bits
                  where bits = L.zipWith bitValue (L.repeat timestamp) [0..ledCount-1]
                        timestamp = round pt
-                       color True = colorByTimeOfDay pt
+                       color True = colorByTimeOfDay startTime endTime pt
                        color False = Color 0 0 0
+                       startTime = Color 255 127 0
+                       endTime = Color 0 0 255
 
 endianness :: Endianness -> [a] -> [a]
 endianness Big = L.reverse
